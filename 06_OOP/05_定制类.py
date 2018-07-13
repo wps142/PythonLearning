a52bc266e5d4715d0830d8f0b72dacb84be08f1e
wps142 <wangps142@126.com>
2018-07-13 18:01:55 +0800

    commit by 180713

--- 06_OOP/05_定制类.py
+++ 06_OOP/05_定制类.py
@@ -49,3 +49,127 @@ class Fib(object):
             raise StopIteration()
         return self.a # 返回下一个值
 
+# __getitem__
+# Fib实例虽然能作用于for循环，看起来和list有点像，但是，把它当成list来使用还是不行
+# 要表现得像list那样按照下标取出元素，需要实现__getitem__()方法：
+class Fib2(object):
+    def __getitem__(self, n):
+        a, b = 1, 1
+        for x in range(n):
+            a, b = b, a + b
+        return a
+
+# 现在，就可以按下标访问数列的任意一项了：
+f=Fib2()
+print(f[2])
+print(f[20])
+# __getitem__()传入的参数可能是一个int，也可能是一个切片对象slice，所以要做判断：
+class Fib3(object):
+    def __getitem__(self, item):
+        if isinstance(item,int):
+            a,b=1,1
+            for x in range(n):
+                a,b=b,a+b
+            return a
+        if isinstance(item,slice):
+            start = item.start
+            end = item.stop
+            if start is None:
+                start = 0
+            a,b=1,1
+            L= []
+
+            for x in range(end):
+                if x>=start:
+                    L.append(a)
+                a,b=b,a+b
+            return L
+
+f2=Fib3()
+print(f2[0:4])
+# 但是没有对step参数作处理：
+# >>> f[:10:2]
+# [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# 也没有对负数作处理，所以，要正确实现一个__getitem__()还是有很多工作要做的。
+# 此外，如果把对象看成dict，__getitem__()的参数也可能是一个可以作key的object，例如str。
+# 与之对应的是__setitem__()方法，把对象视作list或dict来对集合赋值。最后，还有一个__delitem__()方法，用于删除某个元素。
+# 总之，通过上面的方法，我们自己定义的类表现得和Python自带的list、tuple、dict没什么区别，这完全归功于动态语言的“鸭子类型”，不需要强制继承某个接口。
+
+# __getattr__
+# 正常情况下，当我们调用类的方法或属性时，如果不存在，就会报错。比如定义Student类：
+class Student(object):
+
+    def __init__(self):
+        self.name = 'Michael'
+# 调用name属性，没问题，但是，调用不存在的score属性，就有问题了：
+s = Student()
+print(s.name)
+# print(s.score)
+# AttributeError: 'Student' object has no attribute 'score'
+# 错误信息很清楚地告诉我们，没有找到score这个attribute。
+# 要避免这个错误，除了可以加上一个score属性外，Python还有另一个机制，那就是写一个__getattr__()方法，动态返回一个属性。修改如下：
+
+class Student2(object):
+
+    def __init__(self):
+        self.name = 'Michael'
+
+    def __getattr__(self, attr):
+        if attr=='score':
+            return 99
+
+# 当调用不存在的属性时，比如score，Python解释器会试图调用__getattr__(self, 'score')来尝试获得属性，这样，我们就有机会返回score的值：
+s2=Student2()
+print(s2.score)
+class Student3(object):
+
+    def __getattr__(self, attr):
+        if attr=='age':
+            return lambda: 25
+# 调用方式要变为：
+s3=Student3()
+print(s3.age())
+
+# 注意，只有在没有找到属性的情况下，才调用__getattr__，已有的属性，比如name，不会在__getattr__中查找。
+# 此外，注意到任意调用如s.abc都会返回None，这是因为我们定义的__getattr__默认返回就是None。
+
+# 要让class只响应特定的几个属性，我们就要按照约定，抛出AttributeError的错误：
+class Student4(object):
+
+    def __getattr__(self, attr):
+        if attr=='age':
+            return lambda: 25
+        raise AttributeError('\'Student\' object has no attribute \'%s\'' % attr)
+
+
+# 如果要写SDK，给每个URL对应的API都写一个方法，那得累死，而且，API一旦改动，SDK也要改。
+# 利用完全动态的__getattr__，我们可以写出一个链式调用：
+class Chain(object):
+
+    def __init__(self, path=''):
+        self._path = path
+
+    def __getattr__(self, path):
+        return Chain('%s/%s' % (self._path, path))
+
+    def __str__(self):
+        return self._path
+
+    __repr__ = __str__
+
+print(Chain().status.user.timeline.list)
+
+
+# __call__
+# 一个对象实例可以有自己的属性和方法，当我们调用实例方法时，我们用instance.method()来调用。
+# 能不能直接在实例本身上调用呢？在Python中，答案是肯定的。
+# 任何类，只需要定义一个__call__()方法，就可以直接对实例进行调用。
+class People(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __call__(self):
+        print('My name is %s.' % self.name)
+
+p=People('aaaaa')
+print(p())
